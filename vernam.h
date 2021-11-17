052ef67d2b85fd626c57d9b3ab7d966d9c74e126
ShpakovDmitry <work.dmitry.shpakov@gmail.com>
2021-11-17 23:57:27 +0200

    added cypher method declaration

--- vernam.h
+++ vernam.h
@@ -8,11 +8,22 @@
 #ifndef __VERNAM_H__
 #define __VERNAM_H__
 
+#include <stddef.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/* 'data' is rewritten with encrypted/decrypted data */
+/* 'len' is data and key length in bytes */
+typedef struct {
+    size_t len;
+    char* data;
+    char* key;
+} VernamData;
 
+/* this method simply does (data ^ key) operation */
+void Vernam_crypt(VernamData* data);
 
 #ifdef __cplusplus
 }
