8ee8d84af7f4ad875e6c3fd1d6f237a23523cc16
ShpakovDmitry <work.dmitry.shpakov@gmail.com>
2021-11-17 22:25:43 +0200

    basic header file created

---
+++ vernam.h
@@ -0,0 +1,21 @@
+/*
+ * file:   vernam.h 
+ * author: Shpakov Dmitry
+ * date:   2021-11-17
+ *
+ */
+
+#ifndef __VERNAM_H__
+#define __VERNAM_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif//__VERNAM_H__
